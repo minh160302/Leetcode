10802bb682711670596112e5e3757731a45a7714
Minh Nguyen <44615298+minh160302@users.noreply.github.com>
2023-09-08 20:19:14 -0400

    Time: 1 ms (72.35%), Space: 39.8 MB (50.95%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,15 +1,12 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
         int[] dp = new int[target + 1];
-        dp[0] = 0;
-        for (int num : nums) {
-            if (num <= target) {
-                dp[num] = 1;
-            }
-        }
         for (int i = 1; i <= target; i++) {
             for (int num : nums) {
-                if (i >= num) {
+                if (i == num) {
+                    dp[i]++;
+                }
+                else if (i > num) {
                     dp[i] += dp[i - num];
                 }
             }
