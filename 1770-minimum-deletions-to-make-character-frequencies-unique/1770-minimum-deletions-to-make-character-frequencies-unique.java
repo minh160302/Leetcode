0cf83e4b518255c49045b90dc8150e5db342f613
Minh Nguyen <44615298+minh160302@users.noreply.github.com>
2023-09-11 21:09:05 -0400

    Time: 8 ms (82.34%), Space: 44.8 MB (13.38%) - LeetHub

---
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minDeletions(String s) {
+        int[] map = new int[26];
+        for (char c : s.toCharArray())
+            map[c - 'a']++;
+        Arrays.sort(map);
+        HashSet<Integer> set = new HashSet<>();
+        int res = 0;
+        for (int i = 0; i < 26; i++) {
+            while (map[i] > 0 && set.contains(map[i])) {
+                map[i]--;
+                res++;
+            }
+            set.add(map[i]);
+        }
+        return res;
+    }
+}
